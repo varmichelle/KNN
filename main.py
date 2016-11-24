5f1cab6a8eae3111b32cf03c5029d0dc6d6f5383
Michelle Li <michellecarolynclark@gmail.com>
2016-11-23 17:03:14 -0800

    quick bug fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Y = iris.target
 from sklearn.cross_validation import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = .75)
 
-form sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
 classifier = KNeighborsClassifier()
 
 classifier.fit(X_train, Y_train)
