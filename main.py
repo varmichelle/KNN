acb4ac2d04d8fa5e35e3b81bb4494f9bf6c9302c
Michelle Li <michellecarolynclark@gmail.com>
2016-11-24 09:34:52 -0800

    knn

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
+from scipy.spatial import distance
 
 class KNN():
     def fit(self, X_train, Y_train):
@@ -8,9 +8,19 @@ class KNN():
     def predict(self, X_test):
         predictions = []
         for row in X_test:
-            label = random.choice(self.Y_train)
+            label = self.closest(row)
             predictions.append(label)
         return predictions
+
+    def closest(self, row):
+        best_dist = distance.euclidean(row, self.X_train[0])
+        best_index = 0
+        for i in range(1, len(self.X_train)):
+            dist = distance.euclidean(row, self.X_train[i])
+            if dist < best_dist:
+                best_dist = dist
+                best_index = i
+        return self.Y_train[best_index]
 
 from sklearn import datasets
 iris = datasets.load_iris()
