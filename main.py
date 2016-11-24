96eabc841e6a8cea98e98533565067542bf5c004
Michelle Li <michellecarolynclark@gmail.com>
2016-11-23 17:56:48 -0800

    random classifier -- will replace

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import random
+
+class KNN():
+    def fit(self, X_train, Y_train):
+        self.X_train = X_train
+        self.Y_train = Y_train
+
+    def predict(self, X_test):
+        predictions = []
+        for row in X_test:
+            label = random.choice(self.Y_train)
+            predictions.append(label)
+        return predictions
+
 from sklearn import datasets
 iris = datasets.load_iris()
 
@@ -8,7 +22,7 @@ from sklearn.cross_validation import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = .75)
 
 from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier()
+classifier = KNN()
 
 classifier.fit(X_train, Y_train)
 
